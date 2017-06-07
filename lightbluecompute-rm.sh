c8f7735284e47d2ea4fa8294a58d936d8fe2e09f
Budi Darmawan <vbudi@yahoo.com>
2017-06-07 13:57:51 -0400

    adding global configuration

---
+++ lightbluecompute-rm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+printf "IBMid:"
+read userid
+printf "Password:"
+stty -echo
+read password
+stty echo
+starttime=`date`
+
+bx login -a api.ng.bluemix.net -u "$userid" -p "$password" | tee login.out
+logerr=`grep FAILED login.out | wc -l`
+rm login.out
+if [ $logerr -eq 1 ]; then
+  echo "#    Login failed... Exiting"
+  exit
+fi
+
+suffix=`echo -e $userid | tr -d '@_.-' | tr -d '[:space:]'`
+
+bx cs init
+clusterName=`bx cs clusters | grep normal | awk '{print $1}'`
+
+cfg=`bx cs cluster-config $clusterName | grep KUBECONFIG`
+$cfg
+
+extIP=`bx cs workers $clusterName | grep Ready | awk '{print $2}'`
+
+if [ -z "$clusterName" ]; then
+  echo "#  cluster not found - create one and wait until the status is normal"
+  exit
+fi
+echo "#  Deploying to $clusterName #"
+echo "#  Mapped IP: $extIP         #"
+bx service key-delete -f cloudantdb cred
+bx service delete -f cloudantdb
+
+cfg=`bx cs cluster-config $clusterName | grep KUBECONFIG`
+$cfg
+kubectl delete secret binding-cloudantdb
+kubectl delete -f lightbluecompute-min.yml
+
+bx app delete -f lightblue-web-${suffix}
--- lightbluecompute.sh
+++ lightbluecompute.sh
@@ -8,7 +8,7 @@ read password
 stty echo
 starttime=`date`
 
-bx login -a api.$region.bluemix.net -u "$userid" -p "$password" | tee login.out
+bx login -a api.ng.bluemix.net -u "$userid" -p "$password" | tee login.out
 logerr=`grep FAILED login.out | wc -l`
 rm login.out
 if [ $logerr -eq 1 ]; then
@@ -19,13 +19,15 @@ fi
 suffix=`echo -e $userid | tr -d '@_.-' | tr -d '[:space:]'`
 
 bx cs init
-clusterName=`bx cs clusters | grep normal | awk 'print $4'`
+clusterName=`bx cs clusters | grep normal | awk '{print $1}'`
 
-if [ $clusterName -eq "" ]; then
+if [ -z "$clusterName" ]; then
   echo "#  cluster not found - create one and wait until the status is normal"
   exit
 fi
-
+echo "#  Deploying to $clusterName #"
+extIP=`bx cs workers $clusterName | grep Ready | awk '{print $2}'`
+echo "# Mapped IP: $extIP #"
 bx service create cloudantNoSQLDB Lite cloudantdb
 bx service key-create cloudantdb cred
 
@@ -34,12 +36,16 @@ bx cs cluster-service-bind $clusterName default cloudantdb
 cfg=`bx cs cluster-config $clusterName | grep KUBECONFIG`
 $cfg
 kubectl create -f lightbluecompute-min.yml
-extIP=`bx cs workers $clusterName | grep Ready | awk '{print $2}'`
+sleep 30
+mysql -udbuser -pPass4dbUs3R -h${extIP} -P30006 < mysql/scripts/load-data.sql
+
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"username": "foo", "password": "bar", "firstName": "foo", "lastName": "bar", "email": "foo@bar.com"}' -i http://${extIP}:30110/micro/customer
+
 cd  web-app-lite
 # edit manifest.yml
 sed -i -e 's/lightblue-web-bmxedu/lightblue-web-'$suffix'/g' manifest.yml
 # 
 # edit config/default.json
-sed -i -e 's/50.23.5.233/'$extIP'/g' config/default.json
+sed -i -e 's/184.172.233.10/'$extIP'/g' config/default.json
 
 bx app push
